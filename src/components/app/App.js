340a53f4fa63609dabade24fe66ff1e6fec0c875
Eugene <clackd1amond@users.noreply.github.com>
2021-10-04 22:20:11 +0300

    page 404

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,27 +1,29 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-import {MainPage, ComicsPage} from '../pages';
-import AppHeader from "../appHeader/AppHeader";
+import { MainPage, ComicsPage, Page404 } from '../pages';
+import AppHeader from '../appHeader/AppHeader';
 
 const App = () => {
-    
-    return (
-        <Router>
-            <div className="app">
-                <AppHeader/>
-                <main>
-                    <Switch>
-                        <Route exact path="/">
-                            <MainPage/>
-                        </Route>
-                        <Route exact path="/comics">
-                            <ComicsPage/>
-                        </Route>
-                    </Switch>
-                </main>
-            </div>
-        </Router>
-    )
-}
+	return (
+		<Router>
+			<div className='app'>
+				<AppHeader />
+				<main>
+					<Switch>
+						<Route exact path='/'>
+							<MainPage />
+						</Route>
+						<Route exact path='/comics'>
+							<ComicsPage />
+						</Route>
+						<Route to='*'>
+							<Page404 />
+						</Route>
+					</Switch>
+				</main>
+			</div>
+		</Router>
+	);
+};
 
 export default App;
